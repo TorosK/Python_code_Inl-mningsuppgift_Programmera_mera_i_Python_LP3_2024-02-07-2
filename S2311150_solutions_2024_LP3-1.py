05b7d5b6a25946da00ceccdcc28e4a79d1ae03f0
Toros <toros.kutlu@claraconsulting.se>
2024-02-22 13:31:12 +0100

    first commit

---
+++ S2311150_solutions_2024_LP3-1.py
@@ -0,0 +1,68 @@
+import pandas as pd
+
+# ------------------------------------------------------------------------------------------------------------------------
+# Uppgift 1
+# Skapa DataFrame-objekten df_cpi, df_regions och df_inflation som innehåller en kopia av innehållet i csv-filerna cpi.csv, regions.csv och inflation.csv. Innehållet i dessa DataFrame-objekt ska du sedan använda för att lösa nedanstående uppgifter.------------------------------------------------------------------------------------------------------------------------
+# Skriv din kod här:
+
+# Load CPI data into a DataFrame
+df_CPI = pd.read_csv('cpi.csv', delimiter=';', encoding='ISO-8859-1')
+
+# Load regional data into a DataFrame
+df_Regions = pd.read_csv('regions.csv', delimiter=';', encoding='ISO-8859-1')
+
+# Load inflation data into a DataFrame
+df_Inflation = pd.read_csv('inflation.csv', encoding='utf-8')
+
+# Merge the regions DataFrame with the CPI DataFrame on the country code
+# This will add 'Land' and 'Kontinent' columns to the CPI data
+df_CPI_merged_with_Regions = pd.merge(df_Regions[['Land', 'Landskod', 'Kontinent']], df_CPI, on='Landskod')
+
+# Display the first few rows of the DataFrame to verify it's correct
+print("printing df_Regions.head()")
+print(df_Regions.head(), '\n')
+
+print("printing df_Inflation.head()")
+print(df_Inflation.head(), '\n')
+
+print("printing df_CPI.head()")
+print(df_CPI.head(), '\n')
+
+# Display the first few rows of the merged DataFrame to verify it's correct
+print("printing df_CPI_merged_with_Regions.head()")
+print(df_CPI_merged_with_Regions.head(), '\n')
+
+
+
+
+# ------------------------------------------------------------------------------------------------------------------------
+# Uppgift 2
+# ------------------------------------------------------------------------------------------------------------------------
+# Skriv din kod här:
+
+
+
+
+# ------------------------------------------------------------------------------------------------------------------------
+# Uppgift 3
+# ------------------------------------------------------------------------------------------------------------------------
+# Skriv din kod här:
+
+
+
+
+# ------------------------------------------------------------------------------------------------------------------------
+# Uppgift 4
+# ------------------------------------------------------------------------------------------------------------------------
+# Skriv din kod här:
+
+
+
+
+# ------------------------------------------------------------------------------------------------------------------------
+# Uppgift 5
+# ------------------------------------------------------------------------------------------------------------------------
+# Skriv din kod här:
+
+
+
